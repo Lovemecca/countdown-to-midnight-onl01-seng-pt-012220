7b0209a6c2be640a345f34353e66265829678e5f
Mecca Jay <mecca.nicolej@gmail.com>
2020-01-29 04:13:33 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
 
-def countdown
+def countdown (integer)
+  countdown = 10
+  while integer > 0
+  puts "#{integer} SECOND(S)!" 
+  integer -= 1
+  end
+ "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep (n)
+  sleep (5) 
 end
